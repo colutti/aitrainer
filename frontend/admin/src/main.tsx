784d820b74734917ec16b4268c619887874fc3a5
Rafael Colucci <rafacolucci@gmail.com>
2026-02-28 16:41:01 +0100

    fix(admin): finalize react deduplication and initialize shared i18n

--- frontend/admin/src/main.tsx
+++ frontend/admin/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
+import '../../src/i18n';
 import App from './App';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
